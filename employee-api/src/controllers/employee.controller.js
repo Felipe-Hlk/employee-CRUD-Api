5a3618354d7d60a9eaefba8b90e67b9a0cc243e8
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-10-12 23:50:47 -0300

    Corrigindo controller e funcionando conex com db

--- employee-api/src/controllers/employee.controller.js
+++ employee-api/src/controllers/employee.controller.js
@@ -6,27 +6,26 @@
  * descrição: arquivo responsável pela lógica do CRUD (API - Employee)
  * data: 11/10/2023
  * autor: Felipe AP
+ *datas em postgreeSql para fazer a tranformação = SELECT TO_CHAR(NOW() :: DATE, 'dd/mm/yyyy');
  */
 
 const db = require('../config/database');
 
 //=> Método responsável por criar um novo 'Employee':
 
-exports.createEmployee = async(req, res) => {
-  const{name, job_role, salary, birth, employee_registration} = req.body;
-  const{rows} = await db.query(
-    "INSERT INTO emploee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)"
+exports.createEmployee = async (req, res) => {
+  const { name, job_role, salary, birth, employee_registration } = req.body;
+  const { rows } = await db.query(
+    "INSERT INTO employee (name, job_role, salary, birth, employee_registration) VALUES ($1, $2, $3, $4, $5)",
     [name, job_role, salary, birth, employee_registration]
   );
 
   res.status(201).send({
-    message: 'Employee added sucessfully!',
+    message: 'Employee added successfully!',
     body: {
-      employee: { name, job_role, salary, birth, employee_registration }
+      employee: { name, job_role, salary, birth, employee_registration },
     },
   });
-    
-  
 };
 
 
--- employee-api/src/routes/employee.routes.js
+++ employee-api/src/routes/employee.routes.js
@@ -13,7 +13,7 @@ const employeeController = require('../controllers/employee.controller');
 
 // => Definindo as rotas do CRUD - 'Employee'
 
-// => Rota responsável por criar um novo 'Colaborador(a)': (Post): localhost:3000/api/emplyees
+// => Rota responsável por criar um novo 'Colaborador(a)': (Post): localhost:3000/api/employees
 
 router.post('/employees', employeeController.createEmployee);
 
