bf1aaa304c7b7222e8afd9df0f22b086809535df
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-10-17 16:18:29 -0300

    Update employee-add.component.ts

--- front-angular/src/app/employee-add/employee-add.component.ts
+++ front-angular/src/app/employee-add/employee-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-add',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-add.component.scss']
 })
 export class EmployeeAddComponent {
+
+  employeeForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder ){
+    this.createForm();
+  }
+
+  createForm() {
+    this.employeeForm = this.formBuilder.group({
+
+      employeeName: ['', Validators.required],
+      job_role: ['', Validators.required],
+      salary: ['', Validators.required],
+      birth: ['', Validators.required],
+      employee_registration: ['', Validators.required]
+
+    });
+
+  }
 
 }
