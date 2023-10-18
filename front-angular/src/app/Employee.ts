95df994ff2d9a163a235a7738e18349b3b20255f
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-10-18 16:16:48 -0300

    Desenvolv classe employee.ts

--- front-angular/src/app/Employee.ts
+++ front-angular/src/app/Employee.ts
@@ -4,3 +4,15 @@
  * data: 18/10/2023
  * autor: Felipe AP
  */
+
+export default class Employee {
+  employeeName!: string;
+  job_role!: string;
+  salary!: number;
+  birth!: Date;
+  employee_registration!: number;
+
+}
+
+
+
